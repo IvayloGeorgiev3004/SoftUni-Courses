95a9af6cefaef0fec11dd9f8d6ce98dbfc3ef75c
IvayloGeorgiev3004 <ivaylo.georgiev3004@gmail.com>
2023-01-21 16:20:25 +0100

    PRIME NUMBERS!!!
    How to find if number is PRIME!!!!

--- JavaScript Fundamentals January 2023/9. Exercise Data Types and Variables/More exercises/02. primeNumberChecker.js
+++ JavaScript Fundamentals January 2023/9. Exercise Data Types and Variables/More exercises/02. primeNumberChecker.js
@@ -1,8 +1,10 @@
-function primeNumberChecker(num){
-    if (num % num === 0 && num % 1 === 0){
-        console.log(`prime`)
-    } else {
-        console.log("not prime")
+function primeNumberChecker(num) {
+    isPrime = true;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            isPrime = false;
+        }
     }
+    console.log(isPrime === true ? "true" : "false")
 }
 primeNumberChecker(11)
