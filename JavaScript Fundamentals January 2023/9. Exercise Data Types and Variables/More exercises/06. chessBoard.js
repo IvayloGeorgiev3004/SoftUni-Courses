87620a0a59063b6b7b25a8219703325bfb2c9552
IvayloGeorgiev3004 <ivaylo.georgiev3004@gmail.com>
2023-01-21 18:57:54 +0100

    Update 06. chessBoard.js

--- JavaScript Fundamentals January 2023/9. Exercise Data Types and Variables/More exercises/06. chessBoard.js
+++ JavaScript Fundamentals January 2023/9. Exercise Data Types and Variables/More exercises/06. chessBoard.js
@@ -5,22 +5,38 @@ function chessBoard(size) {
         for (let j = 1; j <= size; j++) {
             if (i % 2 === 0) {
                 if (j % 2 === 0) {
-                    console.log(`    <span class="black"></span>`)
+                    console.log(`\t<span class="black"></span>`)
                 } else {
-                    console.log(`    <span class="white"></span>`)
+                    console.log(`\t<span class="white"></span>`)
                 }
             } else if (i % 2 !== 0) {
                 if (j % 2 === 0) {
-                    console.log(`    <span class="white"></span>`)
+                    console.log(`\t<span class="white"></span>`)
                 } else {
-                    console.log(`    <span class="black"></span>`)
+                    console.log(`\t<span class="black"></span>`)
                 }
             }
         }
 
-        console.log(`  </div>`)
+        console.log(`\t</div>`)
     }
     console.log(`</div>`)
 
 }
 chessBoard(3)
+
+/* function chessBoard2(size) {
+    let buffer = ""
+    console.log(`<div class="chessboard">`)
+    for (let i = 1; i <= size; i++) {
+        buffer+= "<div>\n"
+        for (let j = 1; j <= size; j++) {
+            buffer+= "\t<span><\\span>\n"
+        }
+        console.log(buffer)
+       
+    }
+    console.log(`</div>`)
+
+}
+chessBoard2(3) */
