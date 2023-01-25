ced8200d1980cf5c0899c14f921919f0fbfca6ad
Ivaylo Georgiev <ivaylo.georgiev3004@gmail.com>
2023-01-25 20:28:35 +0100

    Update 09. specialNumbers3.js

--- JavaScript Fundamentals January 2023/8. Data Types and Variables/09. specialNumbers3.js
+++ JavaScript Fundamentals January 2023/8. Data Types and Variables/09. specialNumbers3.js
@@ -1,21 +1,21 @@
 function specialNumbers3(number) {
-    let stringFromNum = number.toString() // числото става стринг
+
 
     for (let i = 1; i <= number; i++) {
         let testString = i.toString()
         let sum = 0;
         for (let j = 0; j < testString.length; j++) {
             let fuckIt = testString[j]
-            fuckIt = Number(fuckIt[j])
-            sum+= fuckIt
+            fuckIt = Number(fuckIt)
+            sum += fuckIt
 
         }
 
-        if (sum === 5 || sum === 7 || sum === 11){
+        if (sum === 5 || sum === 7 || sum === 11) {
             console.log(`${i} -> True`)
         } else {
             console.log(`${i} -> False`)
         }
     }
 }
-specialNumbers3(15)
+specialNumbers3(20)
