99eb860e180a7ced5aeb19e2041c431b7188e45d
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-01-26 19:34:40 +0100

    Update 07. equalArrays.js

--- JavaScript Fundamentals January 2023/11. Arrays/07. equalArrays.js
+++ JavaScript Fundamentals January 2023/11. Arrays/07. equalArrays.js
@@ -1,15 +1,10 @@
-function equalArrays(array1, array2) {
+/* function equalArrays(array1, array2) {
     let arr1 = 0;
     let arr2 = 0;
     let sum = 0;
     let flag = false;
 
     for (let i = 0; i < array1.length; i++) {
-        /*     if (array1.length !== array2.length) {
-                console.log(`Arrays are not identical. Found difference at ${array1.length} index`)
-                flag = true;
-                break;
-            } */
         array1[i] = Number(array1[i])
         for (let j = i; j < array1.length; j++) {
             array2[j] = Number(array2[j])
@@ -34,5 +29,33 @@ function equalArrays(array1, array2) {
     }
 
 }
+equalArrays(['10', '20'], ['10', '20', '30']) */
 
-equalArrays(['10', '20', '30'], ['10'])
+
+function equalArrays(array1, array2) {
+
+    let areEqual = Boolean;
+    let arrSum = 0;
+
+    for (let i = 0; i < array1.length; i++) {
+        array1[i] = Number(array1[i])
+    }
+    for (let j = 0; j < array1.length; j++) {
+        array2[j] = Number(array2[j])
+    }
+    for (let i = 0; i < array1.length; i++) {
+        if (array1[i] !== array2[i]) {
+            console.log(`Arrays are not identical. Found difference at ${i} index`)
+            areEqual = false;
+            break;
+        } else {
+            areEqual = true;
+            arrSum += array1[i]
+        }
+    }
+    if (areEqual === true) {
+        console.log(`Arrays are identical. Sum: ${arrSum}`)
+    }
+
+}
+equalArrays(['1','2','3','4','5'], ['1'])
