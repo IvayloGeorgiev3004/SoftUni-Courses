68bc91c0a3a5d14885f8ea138ed1d50b58d19c65
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-01-27 20:18:22 +0100

    Update 04. arrayRotation.js

--- JavaScript Fundamentals January 2023/12. Exercise Arrays/04. arrayRotation.js
+++ JavaScript Fundamentals January 2023/12. Exercise Arrays/04. arrayRotation.js
@@ -1,14 +1,35 @@
 function arrayRotation(array, numOfRotations) {
-    let nweArray = []
+    let newArray = []
 
     for (let j = 0; j < numOfRotations; j++) {
-      
-        nweArray.push(array[0])
+
+        newArray.push(array[0])
         array.shift()
-        array.push(nweArray[0])
-        nweArray.shift()
+        array.push(newArray[0])
+        newArray.shift()
     }
     console.log(array.join(" "))
 
 }
 arrayRotation([51, 47, 32, 61, 21], 2)
+
+
+function arrayRotation(array, numRotation) {
+    let newArray = [];
+    let counter = 0;
+
+    while (counter < numRotation) {
+        for (let i = 1; i <= array.length - 1; i++) {
+            newArray.push(array[i]);
+
+        }
+        newArray.push(array[0]);
+        counter++;
+        array = newArray;
+        newArray = [];
+
+    }
+
+
+    console.log(array.join(" "));
+}
