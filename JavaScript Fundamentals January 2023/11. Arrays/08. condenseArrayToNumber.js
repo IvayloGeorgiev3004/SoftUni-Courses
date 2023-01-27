9fa4f59af6a2129c8644981a06b0fd19008b84f3
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-01-27 11:06:42 +0100

    Update 08. condenseArrayToNumber.js

--- JavaScript Fundamentals January 2023/11. Arrays/08. condenseArrayToNumber.js
+++ JavaScript Fundamentals January 2023/11. Arrays/08. condenseArrayToNumber.js
@@ -1,17 +1,22 @@
 function condenseArrayToNumber(array) {
     let newArr = [];
     let buff = 0;
-    let sum1 = 0;
-    let sum2 = 0;
 
-
-    for (let j = 0; j < 1; j++)
+    if (array.length < 2) {
+        newArr = array;
+    }
+    while (array.length > 1) {
         for (let i = 0; i < array.length - 1; i++) {
-            newArr[i] = array[i] + array[i + 1]
-
+            buff = array[i] + array[i + 1]
+            newArr.push(buff);
+            buff = 0;
         }
+        array = newArr
+        newArr = []
+    }
 
-    console.log(newArr)
-    console.log(newArr.length)
+    console.log(array.join(" "))
+
 }
-condenseArrayToNumber([1,2,3])
+condenseArrayToNumber([1])
+
