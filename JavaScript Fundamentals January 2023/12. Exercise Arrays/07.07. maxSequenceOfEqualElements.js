41c5196c391e07c1dc21ec1f772fc0c3864cc81c
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-01-30 07:45:40 +0100

    Update 07.07. maxSequenceOfEqualElements.js

--- JavaScript Fundamentals January 2023/12. Exercise Arrays/07.07. maxSequenceOfEqualElements.js
+++ JavaScript Fundamentals January 2023/12. Exercise Arrays/07.07. maxSequenceOfEqualElements.js
@@ -1,11 +1,23 @@
 function maxSequenceOfEqualElements(array) {
     let newMax = 0;
+    let maxSeq = 1;
+    let resultIndex = 0;
+    let finalResult = ""
 
     for (let i = 0; i < array.length - 1; i++) {
-        if (array[i] === array[i+1]){
-            
+        if (array[i] === array[i + 1]) {
+            maxSeq++
+        } else {
+            maxSeq = 1
+        }
+        if (maxSeq > newMax) {
+            newMax = maxSeq;
+            resultIndex = array[i]
         }
     }
-
+    for (let i = 0; i< newMax;i++){
+        finalResult+= resultIndex + " "
+    }
+    console.log(finalResult)
 }
-maxSequenceOfEqualElements([2, 1, 1, 2, 3, 3, 2, 2, 2, 1])
+maxSequenceOfEqualElements([1, 1, 1, 2, 3, 1, 3, 3])
