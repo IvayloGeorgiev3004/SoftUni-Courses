f700242d1549e3e32bfaff0163a2b07bac660572
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-02-04 13:16:45 +0100

    Update 10.10. ladyBugs.js

--- JavaScript Fundamentals January 2023/12. Exercise Arrays/10.10. ladyBugs.js
+++ JavaScript Fundamentals January 2023/12. Exercise Arrays/10.10. ladyBugs.js
@@ -1,30 +1,44 @@
 function ladyBugs(array) {
-    let fieldSize = array[0]
-    let bugsStartPosition = array.slice(1, 2).join(" ").split(" ")
-    let bugsCommands = array.slice(2)
-    let startingFiled = []
+    let fieldSize = array[0];
+    let bugsStartPositions = array.slice(1, 2).join(" ").split(" ");
+    let bugsCommands = array.slice(2);
+    let startingFiled = [];
 
     for (let i = 0; i < fieldSize; i++) {
-        startingFiled.push(0)
+        startingFiled.push(0);
     }
 
-    for (let bugsPositions of bugsStartPosition) {
-        let addBugsToField = bugsPositions
-        if (addBugsToField < 0 || addBugsToField >= fieldSize || addBugsToField === "") {
+    for (let bugsPositions of bugsStartPositions) {
+        if (bugsPositions < 0 || bugsPositions >= fieldSize || bugsPositions === "") {
             continue;
         }
-        startingFiled[addBugsToField] = 1
+        startingFiled[Number(bugsPositions)] = 1;
     }
 
-    debugger
+    for (let commands of bugsCommands) {
+        let bugsBuffCommands = commands.split(" ")
 
+        let bugFlyFromIndex = Number(bugsBuffCommands[0]);
+        let bugFlyDirection = bugsBuffCommands[1];
+        let bugFlyLength = Number(bugsBuffCommands[2]);
 
+        if (bugFlyFromIndex < 0 || bugFlyFromIndex >= fieldSize || startingFiled[(bugFlyFromIndex)] !== 1) {
+            continue;
+        }
+        if (bugFlyLength + bugFlyFromIndex >= fieldSize) {
+            startingFiled[bugFlyFromIndex] = 0
+            debugger
+            continue;
+        }
+        
+    }
+
+    console.log(startingFiled.join(" "))
 
 }
-ladyBugs([
-    3, '1 0',
-    '0 right 1',
-    '2 right 1'])
+ladyBugs([3, '0 1',
+    '1 right 2',
+    '10 left 1'])
 
 /* function testing (array) {
     let newArray = array[0].split("|")
