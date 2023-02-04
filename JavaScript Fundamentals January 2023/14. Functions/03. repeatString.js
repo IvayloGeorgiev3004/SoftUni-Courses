c85f43e26094d26e7526ee7c7c25f067a06c127a
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-02-04 15:40:34 +0100

    Update 03. repeatString.js

--- JavaScript Fundamentals January 2023/14. Functions/03. repeatString.js
+++ JavaScript Fundamentals January 2023/14. Functions/03. repeatString.js
@@ -1,8 +1,9 @@
 function repeatString(string, repeatTimes) {
     let buffString = ""
     for (let i = 0; i < repeatTimes; i++) {
-        buffString = string
+        buffString += string
     }
     console.log(buffString)
 }
+
 repeatString("abc", 3)
