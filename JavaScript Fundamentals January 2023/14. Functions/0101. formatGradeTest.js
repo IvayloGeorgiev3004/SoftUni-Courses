01f7d1ed101c776ea38640d97aedb5dd01cf755a
Ivaylo Georgiev <ivaylo.georgiev3004@gmail.com>
2023-02-05 10:17:30 +0100

    Create 0101. formatGradeTest.js

---
+++ JavaScript Fundamentals January 2023/14. Functions/0101. formatGradeTest.js
@@ -0,0 +1,27 @@
+function formatGradeTest(grade) {
+    function formatGrade(gradeNum) {
+        let grade = ""
+        if (gradeNum < 3) {
+            grade = "Fail"
+            gradeNum = 2;
+        } else if (gradeNum < 3.5) {
+            grade = "Poor"
+        } else if (gradeNum < 4.5) {
+            grade = "Good"
+        } else if (gradeNum < 5.5) {
+            grade = "Very good"
+        } else if (gradeNum >= 5.5 && gradeNum <= 6) {
+            grade = "Excellent"
+        }
+
+        let gradeValue = gradeNum < 3 ? "2" : gradeNum.toFixed(2)
+        return (`${grade} (${(gradeValue)})`)
+        debugger
+    }
+
+    let result12 = formatGrade(grade)
+    return result12
+} console.log(formatGradeTest(2))
+
+
+/* formatGrade(22) */
