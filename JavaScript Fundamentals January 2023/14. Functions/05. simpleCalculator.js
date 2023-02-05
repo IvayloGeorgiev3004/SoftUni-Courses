6a9ddf8e0b8043bfe694738ca02e45740fe01aec
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-02-05 15:54:56 +0100

    Update 05. simpleCalculator.js

--- JavaScript Fundamentals January 2023/14. Functions/05. simpleCalculator.js
+++ JavaScript Fundamentals January 2023/14. Functions/05. simpleCalculator.js
@@ -1,8 +1,8 @@
 function simpleCalculator(a, b, operator) {
     let result = 0;
     switch (operator) {
-        // case "multiply": result = (a, b) => a * b; return result(a, b); // using arrow operator
-        case "multiply": result = multiplyTwoNumbers(a,b); return result(a, b); // using another function
+         case "multiply": result = (a, b) => a * b; return result(a, b); // using arrow operator
+       /*  case "multiply": result = multiplyTwoNumbers(a,b); return result(a, b); */ // using another function
         case "divide": result = (a, b) => a / b; return result(a, b);  // using arrow operator
         case "add": result = (a, b) => a + b; return result(a, b);  // using arrow operator
         case "subtract": result = (a, b) => a - b; return result(a, b);  // using arrow operator
@@ -27,3 +27,15 @@ function addTwoNumbers(num1, num2) {
 function subtractTwoNumbers(num1, num2) {
     return num1 - num2;
 }
+
+
+function calculator(num1, num2, operator) {
+    let operationsObject = {
+        'add': num1 + num2,
+        'subtract': num1 - num2,
+        'divide': num1 / num2,
+        'multiply': num1 * num2,
+    }
+    console.log(operationsObject[operator]);
+}
+calculator(5,5,"add")
