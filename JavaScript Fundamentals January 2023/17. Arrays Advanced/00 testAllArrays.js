ea5163727f1e49f68852e2b895710c7316d1e25a
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-02-09 21:37:15 +0100

    some tests added

---
+++ JavaScript Fundamentals January 2023/17. Arrays Advanced/00 testAllArrays.js
@@ -0,0 +1,10 @@
+function test(array) {
+    console.log(array);
+   let slicedArray = []
+   slicedArray = array.slice(1)
+   console.log(slicedArray)
+   slicedArray = array.slice(2,3)
+   console.log(slicedArray)
+
+}
+test([`one`, ` two`, 3, `five`])
