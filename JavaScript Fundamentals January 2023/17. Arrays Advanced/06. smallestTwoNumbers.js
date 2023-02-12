bea803367aece63183638cff654d7191ef17c119
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-02-12 05:40:15 +0100

    added the last three ,06,07,08 from array advanced

---
+++ JavaScript Fundamentals January 2023/17. Arrays Advanced/06. smallestTwoNumbers.js
@@ -0,0 +1,8 @@
+function smallestTwoNumbers(array) {
+
+    array.sort((a, b) => a - b)
+    let finalResult = array.slice(0, 2)
+    return finalResult.join(" ")
+
+}
+smallestTwoNumbers([30, 15, 50, 5])
---
+++ JavaScript Fundamentals January 2023/17. Arrays Advanced/07. listOfProducts.js
@@ -0,0 +1,7 @@
+function listOfProducts(array) {
+    array.sort()
+    for (let i = 0; i < array.length; i++) {
+        console.log(`${i + 1}.${array[i]}`)
+    }
+}
+listOfProducts(['Potatoes', 'Tomatoes', 'Onions', 'Apples'])
