b8a40e60f1e485d10b47097d15476b5b7e1912ad
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-02-13 06:15:46 +0100

    Update 05. SortAnArrayBy2Criteria.js

--- JavaScript Fundamentals January 2023/18. Exercise Arrays Advanced/05. SortAnArrayBy2Criteria.js
+++ JavaScript Fundamentals January 2023/18. Exercise Arrays Advanced/05. SortAnArrayBy2Criteria.js
@@ -1,6 +1,21 @@
 function SortAnArrayBy2Criteria(array) {
-    array.sort((a, b) => a.length - b.length)
+    let initialLength = array[0]
+    initialLength = initialLength.length
+    let lengthCheck = true;
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i].length !== initialLength) {
+            lengthCheck = false
+        }
+    }
+
+    if (lengthCheck === true) {
+        array.sort((a, b,) => a.length - b.length);
+    } else {
+        array.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+        array.sort((a, b,) => a.length - b.length);
+    }
 
     console.log(array.join("\n"))
 }
-SortAnArrayBy2Criteria(['Deny', 'omen', 'test', 'Default'])
+SortAnArrayBy2Criteria(['test', 'Deny', 'omen', 'Default'])
