0308c6c7af639926991bdfc8d4bc9ccaf00331cd
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-02-18 21:21:03 +0100

    Update 03. manOWar.js

--- JavaScript Fundamentals January 2023/19. Exam Preparation/03. manOWar.js
+++ JavaScript Fundamentals January 2023/19. Exam Preparation/03. manOWar.js
@@ -20,30 +20,71 @@ function manOWar(array) {
         switch (command) {
             case "Fire": let index = commandPower[0]; let damage = commandPower[1];
                 fireAtWarship(index, damage); break;
-            
-            case "Defend": // TO DO
-            case "Repair": // TO DO
-            case "Status": // TO DO
-            case "Retire": // TO DO
+
+            case "Defend": let startIndex = commandPower[0]; let endIndex = commandPower[1]; let WarShipDamage = commandPower[2]
+                defendMyShip(startIndex, endIndex, WarShipDamage); break;
+
+            case "Repair": let indexToRepair = commandPower[0]; let healthToRepair = commandPower[1];
+                repairMyShip(indexToRepair, healthToRepair); break;
+
+            case "Status": statusOfShipSections(pirateShip,maxHealth); break;
+            case "Retire":
+                console.log(`Pirate ship status: ${pirateShip.reduce((a, b) => a + b, 0)}`)
+                console.log(`Warship status: ${warShip.reduce((a, b) => a + b, 0)}`)
+                break;
         }
-        debugger
+        if (isShipDead(pirateShip)) {
+            console.log("You lost! The pirate ship has sunken.")
+            break;
+        }
+
+        if (isShipDead(warShip)) {
+            console.log("You won! The enemy ship has sunken.")
+        }
+
     }
 
     function fireAtWarship(index, damage) {
-        if (index >= 0 && index <= warShip.length) {
+        if (index >= 0 && index < warShip.length) {
             warShip[index] -= Number(damage)
         }
 
     }
 
+    function defendMyShip(startIndex, endIndex, damage) {
+        if (startIndex >= 0 && startIndex < pirateShip.length && endIndex >= 0 && endIndex < pirateShip.length) {
+            for (let i = startIndex; i <= endIndex; i++) {
+                pirateShip[i] -= damage;
+            }
+        }
+    }
+
+    function repairMyShip(index, healthToRepair) {
+        if (index >= 0 && index < pirateShip.length) {
+            let sectionHealth = pirateShip[index]
+            let minHealth = maxHealth - sectionHealth
+            let healthToUse = Math.min(minHealth, healthToRepair)
+            pirateShip[index] += Number(healthToUse)
+        }
+    }
+
+    function statusOfShipSections(pirateShip, maxHealth) {
+        let sectionsForRepair = pirateShip.filter(a => a < (maxHealth * 0.2));
+        debugger
+        console.log(`${sectionsForRepair.length} sections need repair.`)
+    }
+
+    function isShipDead(ship) {
+        let isDead = ship.filter(a => a <= 0)
+        return isDead.length > 0
+    }
+
 }
-manOWar(["12>13>11>20>66",
-    "12>22>33>44>55>32>18",
-    "70",
-    "Fire 2 11",
-    "Fire 8 100",
-    "Defend 3 6 11",
-    "Defend 0 3 5",
-    "Repair 1 33",
+manOWar(["2>3>4>5>2",
+    "6>7>2>9>10>11",
+    "20",
     "Status",
+    "Fire 2 3",
+    "Defend 0 4 11",
+    "Repair 3 18",
     "Retire"])
