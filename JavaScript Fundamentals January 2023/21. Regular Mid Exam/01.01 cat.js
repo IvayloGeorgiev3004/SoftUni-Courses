18c1a2f0005ffa76c8242b43710d25ba0e962a72
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-02-23 21:17:42 +0100

    Update 01.01 cat.js

--- JavaScript Fundamentals January 2023/21. Regular Mid Exam/01.01 cat.js
+++ JavaScript Fundamentals January 2023/21. Regular Mid Exam/01.01 cat.js
@@ -1,52 +1,52 @@
-// function cat(array, entryIndex, typeOfItems) {
+function cat(array, entryIndex, typeOfItems) {
 
-//     switch (typeOfItems) {
-//         case "cheap": console.log(checkCheap(array, entryIndex)); break;
-//         case "expensive": console.log(checkExpensive(array, entryIndex)); break;
-//     }
+    switch (typeOfItems) {
+        case "cheap": console.log(checkCheap(array, entryIndex)); break;
+        case "expensive": console.log(checkExpensive(array, entryIndex)); break;
+    }
 
-//     function checkCheap(array, entryIndex) {
-//         let priceTag = Number(array[entryIndex])
-//         let rightPrice = 0;
-//         let leftPrice = 0;
-//         for (let i = entryIndex + 1; i < array.length; i++) {
-//             if (Number(array[i]) < priceTag)
-//                 rightPrice += Number(array[i])
-//         }
-//         for (let j = 0; j < entryIndex; j++) {
-//             if (Number(array[j]) < priceTag)
-//                 leftPrice += Number(array[j])
-//         }
-//         if (leftPrice >= rightPrice) {
-//             return `Left - ${leftPrice}`
+    function checkCheap(array, entryIndex) {
+        let priceTag = Number(array[entryIndex])
+        let rightPrice = 0;
+        let leftPrice = 0;
+        for (let i = entryIndex + 1; i < array.length; i++) {
+            if (Number(array[i]) < priceTag)
+                rightPrice += Number(array[i])
+        }
+        for (let j = 0; j < entryIndex; j++) {
+            if (Number(array[j]) < priceTag)
+                leftPrice += Number(array[j])
+        }
+        if (leftPrice >= rightPrice) {
+            return `Left - ${leftPrice}`
 
-//         } else {
-//             return `Right - ${rightPrice}`
-//         }
-//     }
+        } else {
+            return `Right - ${rightPrice}`
+        }
+    }
 
-//     function checkExpensive(array, entryIndex) {
-//         let priceTag = Number(array[entryIndex])
-//         let rightPrice = 0;
-//         let leftPrice = 0;
-//         for (let i = entryIndex + 1; i < array.length; i++) {
-//             if (Number(array[i]) >= priceTag)
-//                 rightPrice += Number(array[i])
-//         }
-//         for (let j = 0; j < entryIndex; j++) {
-//             if (Number(array[j]) >= priceTag)
-//                 leftPrice += Number(array[j])
-//         }
-//         if (leftPrice >= rightPrice) {
-//             return "Left - " + leftPrice
-//         } else {
-//             return "Right - " + rightPrice
-//         }
+    function checkExpensive(array, entryIndex) {
+        let priceTag = Number(array[entryIndex])
+        let rightPrice = 0;
+        let leftPrice = 0;
+        for (let i = entryIndex + 1; i < array.length; i++) {
+            if (Number(array[i]) >= priceTag)
+                rightPrice += Number(array[i])
+        }
+        for (let j = 0; j < entryIndex; j++) {
+            if (Number(array[j]) >= priceTag)
+                leftPrice += Number(array[j])
+        }
+        if (leftPrice >= rightPrice) {
+            return "Left - " + leftPrice
+        } else {
+            return "Right - " + rightPrice
+        }
 
-//     }
+    }
 
-// }
-// cat([5, 10, 12, 5, 4, 20], 3, "cheap")
+}
+cat([5, 10, 12, 5, 4, 20], 3, "cheap")
 
 
 function catTest(arr, index, itemsValue) {
@@ -63,25 +63,21 @@ function catTest(arr, index, itemsValue) {
             let rightSidePrice = rightSide.filter(element => element < priceTag)
             finalSumLeft = leftSidePrice.reduce((a, b) => a + b, 0)
             finalSumRight = rightSidePrice.reduce((a, b) => a + b, 0)
-            debugger
             break;
         case "expensive":
             let leftSidePriceExp = leftSide.filter(element => element >= priceTag)
-            debugger
             let rightSidePriceExp = rightSide.filter(element => element >= priceTag)
             finalSumLeft = leftSidePriceExp.reduce((a, b) => a + b, 0)
             finalSumRight = rightSidePriceExp.reduce((a, b) => a + b, 0)
-            debugger
             break;
     }
 
-    if (finalSumLeft >= finalSumRight){
+    if (finalSumLeft >= finalSumRight) {
         console.log(`Left - ${finalSumLeft}`)
     } else {
         console.log(`Right - ${finalSumRight}`)
     }
 
-    debugger
 
 }
-catTest([-2,2,1,5,9,3,2-2,1,-1,-3,3], 7, "expensive")
+catTest([-2, 2, 1, 5, 9, 3, 2 - 2, 1, -1, -3, 3], 7, "expensive")
