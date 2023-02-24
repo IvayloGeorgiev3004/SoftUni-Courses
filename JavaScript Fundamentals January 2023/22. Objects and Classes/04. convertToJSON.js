22ce59ff9f103983c58c16fd003bd2739b7ebcef
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-02-24 20:22:29 +0100

    Create 04. convertToJSON.js

---
+++ JavaScript Fundamentals January 2023/22. Objects and Classes/04. convertToJSON.js
@@ -0,0 +1,12 @@
+function convertToJSON(name, lastName, hairColor){
+    let personInfo = {
+        name,
+        lastName,
+        hairColor,
+    }
+    let JSONfromObject = JSON.stringify(personInfo)
+    console.log(JSONfromObject)
+    debugger
+
+}
+convertToJSON('Peter', 'Smith', 'Blond')
