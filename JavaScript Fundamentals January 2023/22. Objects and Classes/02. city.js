b46f62a87ccc7100adbd15b22b57846dde2251fd
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-02-24 20:22:25 +0100

    Update 02. city.js

--- JavaScript Fundamentals January 2023/22. Objects and Classes/02. city.js
+++ JavaScript Fundamentals January 2023/22. Objects and Classes/02. city.js
@@ -1,27 +1,17 @@
-function city(object){
+function city(object) {
 
     // console.log(Object.keys(object))
     // console.log(Object.entries(object))
+    // console.log(Object.values(object))
 
-    for (values of Object.entries(object)){
-        console.log(values)
-    }
-    console.log(`--------------------------------`)
-    for (values of Object.keys(object)){
-
-        debugger
-        console.log(object[values])
-
-        debugger
-    }
-    console.log(`--------------------------------`)
-    for (values of Object.values(object)){
-        console.log(values)
+    for (values of Object.keys(object)) {
+        console.log(`${values} -> ${object[values]}`)
     }
 
 }
 city({
 
     name: "Sofia",
-    
-    area: 492,population: 1238438, country: "Bulgaria", postCode: "1000" })
+
+    area: 492, population: 1238438, country: "Bulgaria", postCode: "1000"
+})
