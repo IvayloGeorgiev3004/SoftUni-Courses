37e10e6c8fbb46805ddd2a5afd757680269d1806
Ivaylo Georgiev <ivaylo.georgiev3004@gmail.com>
2023-02-24 18:05:22 +0100

    Update 01. personInfo.js

--- JavaScript Fundamentals January 2023/22. Objects and Classes/01. personInfo.js
+++ JavaScript Fundamentals January 2023/22. Objects and Classes/01. personInfo.js
@@ -1,21 +1,23 @@
-// function personInfo(firstName, lastName, age) {
-//     let myPersonInfo = {};
-//     myPersonInfo.firstName = firstName;
-//     myPersonInfo.lastName = lastName;
-//     myPersonInfo.age = age;
+function personInfo(firstName, lastName, age) {
+    let myPersonInfo = {};
+    myPersonInfo.firstName = firstName;
+    myPersonInfo.lastName = lastName;
+    myPersonInfo.age = age;
 
-//     return myPersonInfo; 
+    return myPersonInfo; 
 
-// }
+}
 personInfo("Peter", "Pan", "20")
 
 function personInfo2(firstName, lastName, age) {
-    let myPersonInfo = {
-        "firstName": {firstName}
+    let personInfo = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
     }
   
 
-    return myPersonInfo; 
+    return personInfo; 
 
 }
 personInfo2("Peter", "Pan", "20")
