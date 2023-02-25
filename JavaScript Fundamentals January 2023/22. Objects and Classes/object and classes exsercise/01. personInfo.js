110255a0db285ebd6c3827163ab667b9114c8ffc
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-02-25 15:26:43 +0100

    Create 01. personInfo.js

---
+++ JavaScript Fundamentals January 2023/22. Objects and Classes/object and classes exsercise/01. personInfo.js
@@ -0,0 +1,11 @@
+function personInfo(firstName, lastName, age){
+    let personInformation = {
+        firstName: firstName,
+        lastName: lastName,
+        age: Number(age),
+    }
+    return personInformation
+}
+personInfo("Peter", 
+"Pan",
+"20")
