fe4f208eacb1d809907c511bcbbfe204687f04c1
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-02-25 17:53:58 +0100

    Create 06. songs.js

---
+++ JavaScript Fundamentals January 2023/22. Objects and Classes/object and classes exsercise/06. songs.js
@@ -0,0 +1,35 @@
+function songs(array) {
+    let numberOfSongs = Number(array.shift());
+    let typeOfSongs = array.pop();
+    let finalSongsList = [];
+
+    class Song {
+        constructor(type, name, time) {
+            this.type = type;
+            this.name = name;
+            this.time = time;
+        }
+        callSongNames() {
+            console.log(this.name)
+        }
+    }
+
+    for (let playList of array) {
+        let [type, name, time] = playList.split("_")
+        let songs = new Song(type, name, time)
+        if (typeOfSongs === "all") {
+            songs.callSongNames()
+        } else if (typeOfSongs === songs.type) {
+            songs.callSongNames()
+        }
+        finalSongsList.push(songs)
+    }
+    debugger
+
+}
+songs([4,
+    'favourite_DownTown_3:14',
+    'listenLater_Andalouse_3:24',
+    'favourite_In To The Night_3:58',
+    'favourite_Live It Up_3:48',
+    'listenLater'])
