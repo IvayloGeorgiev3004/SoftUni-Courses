bf0b059d0ebe453f4d5a1050218bd14ce07670b4
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-02-25 16:13:24 +0100

    Update 04. convertToJSON.js

--- JavaScript Fundamentals January 2023/22. Objects and Classes/object and classes exsercise/04. convertToJSON.js
+++ JavaScript Fundamentals January 2023/22. Objects and Classes/object and classes exsercise/04. convertToJSON.js
@@ -1,23 +1,23 @@
-// function convertToJSON(firstName, lastName,hairColor){
-//     class Person {
-//         constructor(firstName, lastName, hairColor) {
-//             this.name = firstName;
-//             this.lastName = lastName;
-//             this.hairColor = hairColor;
-//         }
-//     }
-//     let personInfo = new Person(firstName, lastName, hairColor)
-//     let JSONfromObject = JSON.stringify(personInfo)
-//     console.log(JSONfromObject)
+function convertToJSON(firstName, lastName,hairColor){
+    class Person {
+        constructor(firstName, lastName, hairColor) {
+            this.name = firstName;
+            this.lastName = lastName;
+            this.hairColor = hairColor;
+        }
+    }
+    let personInfo = new Person(firstName, lastName, hairColor)
+    let JSONfromObject = JSON.stringify(personInfo)
+    console.log(JSONfromObject)
 
-// }
-// convertToJSON('George', 'Jones', 'Brown')
+}
+convertToJSON('George', 'Jones', 'Brown')
 
 function convertToJSON2(firstName, lastName,hairColor){
     let object = {
-        firstName,
-        lastName,
-        hairColor,
+        firstName, // може да се използва и firstName: firstName,
+        lastName, // може да се използва и lastName: lastName,
+        hairColor, // може да се използва и hairColor: hairColor,
     }
     let JSONfromObject = JSON.stringify(object)
     console.log(JSONfromObject)
