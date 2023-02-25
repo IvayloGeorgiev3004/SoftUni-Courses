58a57e0f20d472e94b9d04284549f17d9eed768c
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-02-25 15:08:58 +0100

    използване на forEach()

--- JavaScript Fundamentals January 2023/22. Objects and Classes/06. songs.js
+++ JavaScript Fundamentals January 2023/22. Objects and Classes/06. songs.js
@@ -1,32 +1,62 @@
-function songs(array) {
-    let totalNumberOfSongs = Number(array.shift());
-    let typeOfList = array.pop();
-    let newSongsList = []
+// function songs(array) {
+//     let totalNumberOfSongs = Number(array.shift());
+//     let typeOfList = array.pop();
+//     let newSongsList = []
 
-    class Song {
-        constructor(type, name, time) {
-            this.type = type;
-            this.name = name;
-            this.time = time
-        }
-    }
+//     class Song {
+//         constructor(type, name, time) {
+//             this.type = type;
+//             this.name = name;
+//             this.time = time
+//         }
+//     }
 
-    for (let arguments of array) {
-        let [type, name, duration] = arguments.split("_")
-        let song = new Song(type, name, duration)
-        newSongsList.push(song)
-    }
-    for (let i = 0; i < totalNumberOfSongs; i++) {
-        let currentSong = newSongsList[i]
-        if (typeOfList === "all") {
-            console.log(currentSong.name)
-        } else if (currentSong.type === typeOfList) {
-            console.log(currentSong.name)
+//     for (let arguments of array) {
+//         let [type, name, duration] = arguments.split("_")
+//         let song = new Song(type, name, duration)
+//         newSongsList.push(song)
+//     }
+//     for (let i = 0; i < totalNumberOfSongs; i++) {
+//         let currentSong = newSongsList[i]
+//         if (typeOfList === "all") {
+//             console.log(currentSong.name)
+//         } else if (currentSong.type === typeOfList) {
+//             console.log(currentSong.name)
+//         }
+//     }
+// }
+// songs([2,
+//     'like_Replay_3:15',
+//     'ban_Photoshop_3:48',
+//     'all'])
+
+    function songs2(array) {
+        let totalNumberOfSongs = Number(array.shift());
+        let typeOfList = array.pop();
+        let newSongsList = []
+    
+        class Song {
+            constructor(type, name, time) {
+                this.type = type;
+                this.name = name;
+                this.time = time
+            }
         }
+    
+        for (let arguments of array) {
+            let [type, name, duration] = arguments.split("_")
+            let song = new Song(type, name, duration)
+            newSongsList.push(song)
+        }
+      
+        if (typeOfList === "all"){
+            newSongsList.forEach((a) => console.log(a.name))
+            console.log(newSongsList.forEach((a) => a.name))
+        }
+
+    debugger
     }
-debugger
-}
-songs([2,
-    'like_Replay_3:15',
-    'ban_Photoshop_3:48',
-    'all'])
+    songs2([2,
+        'like_Replay_3:15',
+        'ban_Photoshop_3:48',
+        'all'])
