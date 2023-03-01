fe4a732bdae3d59a61a61592d8c28b8bd8d22c5e
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-03-01 16:56:44 +0100

    Update 07. classVehicle.js

--- JavaScript Fundamentals January 2023/23. Exercise Objects and Classes/07. classVehicle.js
+++ JavaScript Fundamentals January 2023/23. Exercise Objects and Classes/07. classVehicle.js
@@ -2,26 +2,30 @@ class Vehicle {
     constructor(type, model, parts, fuel) {
         this.type = type;
         this.model = model;
-        this.parts = parts;
-        this.parts.quality = (this.parts.engine * this.parts.power);
+        this.parts = {
+            engine: parts.engine,
+            power: parts.power,
+            quality: parts.engine * parts.power,
+        }
+
         this.fuel = Number(fuel);
     }
 
-    drive(fuelLos) {
-        this.fuel -= fuelLos;
+    drive(fuelLoss) {
+        this.fuel -= fuelLoss;
     }
 }
-let parts = { engine: 6, power: 100 };
-let vehicle = new Vehicle('a', 'b', parts, 200);
-debugger
-vehicle.drive(100);
-console.log(vehicle.fuel);
-console.log(vehicle.parts.power);
-
-// let parts = {engine: 9, power: 500};
-// let vehicle = new Vehicle('l', 'k', parts, 840);
-// vehicle.drive(20);
+// let parts = { engine: 6, power: 100 };
+// let vehicle = new Vehicle('a', 'b', parts, 200);
+// debugger
+// vehicle.drive(100);
 // console.log(vehicle.fuel);
+// console.log(vehicle.parts.power);
+
+let parts = { engine: 9, power: 500 };
+let vehicle = new Vehicle('l', 'k', parts, 840);
+vehicle.drive(20);
+console.log(vehicle.fuel);
 
 
 
