86a44568485b9269b07bbb923f4ca34e7d905bbb
Ivaylo Georgiev <ivaylo.georgiev3004@gmail.com>
2023-03-01 21:27:30 +0100

    Update 08. classStorage.js

--- JavaScript Fundamentals January 2023/23. Exercise Objects and Classes/08. classStorage.js
+++ JavaScript Fundamentals January 2023/23. Exercise Objects and Classes/08. classStorage.js
@@ -1,21 +1,21 @@
 
  class Storage{
     constructor(capacity) {
-        this.capacity = capacity
+        this.capacity = capacity;
     }
-    totalCost = 0;
     storage = [];
+    totalCost = 0;
 
-    addProduct(product) {
+    addProduct(product){
         this.storage.push(product)
-        this.capacity -= product.quantity
+        this.capacity-= product.quantity
         this.totalCost+= product.price * product.quantity
     }
     getProducts(){
-        return this.storage.map(element => JSON.stringify(element)).join('\n')
+        return this.storage.map(element => JSON.stringify(element)).join("\n")
     }
  }
-
+ 
  let productOne = {name: 'Cucamber', price: 1.50, quantity: 15};
  let productTwo = {name: 'Tomato', price: 0.90, quantity: 25};
  let productThree = {name: 'Bread', price: 1.10, quantity: 8};
