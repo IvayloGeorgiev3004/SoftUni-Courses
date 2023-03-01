81cdeec47177044f28087842b2951262fcf3d9db
Ivaylo Georgiev <ivaylo.georgiev3004@gmail.com>
2023-03-01 18:52:42 +0100

    Update 08. classStorage.js

--- JavaScript Fundamentals January 2023/23. Exercise Objects and Classes/08. classStorage.js
+++ JavaScript Fundamentals January 2023/23. Exercise Objects and Classes/08. classStorage.js
@@ -1,45 +1,50 @@
 
-// class Storage {
-//     constructor(capacity) {
-//         this.capacity = capacity;
-
-//     }
-//     storage = {
-//         name: this.storage.name,
-//         price: this.storage.price,
-//         quantity: this.storage.quantity,
-//     }
-
-// }
-
-
-// let productOne = { name: 'Cucamber', price: 1.50, quantity: 15 };
-// let productTwo = { name: 'Tomato', price: 0.90, quantity: 25 };
-// let productThree = { name: 'Bread', price: 1.10, quantity: 8 };
-// let storage = new Storage(50);
-// storage.addProduct(productOne);
-// storage.addProduct(productTwo);
-// storage.addProduct(productThree);
-// console.log(storage.getProducts());
-// console.log(storage.capacity);
-// console.log(storage.totalCost);
-
-
-
 class Storage {
     constructor(capacity) {
-      this.capacity = capacity;
-      this.storage = [];
-      this.totalCost = 0;
+        this.capacity = capacity;
+        this.storage = [];
+        this.totalCost = 0;
     }
+   addProduct(product){
+    this.storage.push(product)
+    this.capacity -= product.quantity
+    this.totalCost += product.quantity * product.price;
+   }
+
+   getProducts(){
+  return this.storage.map(element => JSON.stringify(element)).join("\n")
+   }
+
+}
+
+
+let productOne = { name: 'Cucamber', price: 1.50, quantity: 15 };
+let productTwo = { name: 'Tomato', price: 0.90, quantity: 25 };
+let productThree = { name: 'Bread', price: 1.10, quantity: 8 };
+let storage = new Storage(50);
+storage.addProduct(productOne);
+storage.addProduct(productTwo);
+storage.addProduct(productThree);
+console.log(storage.getProducts());
+console.log(storage.capacity);
+console.log(storage.totalCost);
+
+
+
+// class Storage {
+//     constructor(capacity) {
+//       this.capacity = capacity;
+//       this.storage = [];
+//       this.totalCost = 0;
+//     }
  
-    addProduct(product) {
-      this.storage.push(product);
-      this.capacity -= product.quantity;
-      this.totalCost += product.price * product.quantity;
-    }
+//     addProduct(product) {
+//       this.storage.push(product);
+//       this.capacity -= product.quantity;
+//       this.totalCost += product.price * product.quantity;
+//     }
  
-    getProducts() {
-      return this.storage.map((product) => JSON.stringify(product)).join('\n');
-    }
-  }
+//     getProducts() {
+//       return this.storage.map((product) => JSON.stringify(product)).join('\n');
+//     }
+//   }
