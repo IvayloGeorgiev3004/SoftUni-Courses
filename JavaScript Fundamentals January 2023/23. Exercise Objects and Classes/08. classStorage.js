9c4b38733e0e45ccdf5a5a61024f00004ef20204
Ivaylo Georgiev <ivaylo.georgiev3004@gmail.com>
2023-03-01 21:14:05 +0100

    Update 08. classStorage.js

--- JavaScript Fundamentals January 2023/23. Exercise Objects and Classes/08. classStorage.js
+++ JavaScript Fundamentals January 2023/23. Exercise Objects and Classes/08. classStorage.js
@@ -1,34 +1,31 @@
 
-class Storage {
+ class Storage{
     constructor(capacity) {
-        this.capacity = capacity;
-        this.storage = [];
-        this.totalCost = 0;
+        this.capacity = capacity
     }
-   addProduct(product){
-    this.storage.push(product)
-    this.capacity -= product.quantity
-    this.totalCost += product.quantity * product.price;
-   }
+    totalCost = 0;
+    storage = [];
 
-   getProducts(){
-  return this.storage.map(element => JSON.stringify(element)).join("\n")
-   }
+    addProduct(product) {
+        this.storage.push(product)
+        this.capacity -= product.quantity
+        this.totalCost+= product.price * product.quantity
+    }
+    getProducts(){
+        return this.storage.map(element => JSON.stringify(element)).join('\n')
+    }
+ }
 
-}
-
-
-let productOne = { name: 'Cucamber', price: 1.50, quantity: 15 };
-let productTwo = { name: 'Tomato', price: 0.90, quantity: 25 };
-let productThree = { name: 'Bread', price: 1.10, quantity: 8 };
-let storage = new Storage(50);
-storage.addProduct(productOne);
-storage.addProduct(productTwo);
-storage.addProduct(productThree);
-console.log(storage.getProducts());
-console.log(storage.capacity);
-console.log(storage.totalCost);
-
+ let productOne = {name: 'Cucamber', price: 1.50, quantity: 15};
+ let productTwo = {name: 'Tomato', price: 0.90, quantity: 25};
+ let productThree = {name: 'Bread', price: 1.10, quantity: 8};
+ let storage = new Storage(50);
+ storage.addProduct(productOne);
+ storage.addProduct(productTwo);
+ storage.addProduct(productThree);
+ console.log(storage.getProducts());
+ console.log(storage.capacity);
+ console.log(storage.totalCost);
 
 
 // class Storage {
