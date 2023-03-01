ef00101792f53576d010f6701ec02e28789197df
Ivaylo Georgiev <ivaylo.georgiev3004@gmail.com>
2023-03-01 21:57:38 +0100

    Update 07.07. classVehicle.js

--- JavaScript Fundamentals January 2023/23. Exercise Objects and Classes/07.07. classVehicle.js
+++ JavaScript Fundamentals January 2023/23. Exercise Objects and Classes/07.07. classVehicle.js
@@ -9,11 +9,11 @@ class Vehicle {
         }
         this.fuel = fuel;
     }
-    
-    
+
+
 }
 
-let parts = { engine: 6, power: 100};
+let parts = { engine: 6, power: 100 };
 let vehicle = new Vehicle('a', 'b', parts, 200);
 // vehicle.drive(100);
 console.log(vehicle.fuel);
