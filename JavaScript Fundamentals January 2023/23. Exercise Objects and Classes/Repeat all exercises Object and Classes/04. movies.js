2fdc0fefd387e8bc33e1030e48bce195a947a880
IvayloGeorgiev3004 <ivaylo.georgiev3004@gmail.com>
2023-03-03 19:47:10 +0100

    Create 04. movies.js

---
+++ JavaScript Fundamentals January 2023/23. Exercise Objects and Classes/Repeat all exercises Object and Classes/04. movies.js
@@ -0,0 +1,45 @@
+function movies(arrayOfCommands) {
+    let moviesArray = []
+
+    arrayOfCommands.forEach(element => {
+
+        if (element.includes(`addMovie`)) {
+            movieName = element.split(`addMovie `)[1]
+            let moviesObj = {};
+            moviesObj.name = movieName
+            moviesArray.push(moviesObj)
+        }
+        if (element.includes(`directedBy`)) {
+            let [movieName, director] = element.split(` directedBy `)
+            moviesArray.map(movies => {
+                if (movies.name === movieName) {
+                    movies.director = director;
+                }
+            })
+        }
+        if (element.includes(`onDate`)) {
+            let [movieName, date] = element.split(` onDate `)
+            moviesArray.map(movies => {
+                if (movies.name === movieName) {
+                    movies.date = date;
+                }
+            })
+        }
+
+    });
+    debugger
+   moviesArray.forEach(movie => {
+    if (movie.name && movie.date && movie.director){
+        console.log(JSON.stringify(movie))
+    }
+   })
+}
+
+movies([
+    'addMovie The Avengers',
+    'addMovie Superman',
+    'The Avengers directedBy Anthony Russo',
+    'The Avengers onDate 30.07.2010',
+    'Captain America onDate 30.07.2010',
+    'Captain America directedBy Joe Russo'
+])
