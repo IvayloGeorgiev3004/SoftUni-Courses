6c904c1963b0214101fde6973b7a7c74f6aac1cb
IvayloGeorgiev3004 <ivaylo.georgiev3004@gmail.com>
2023-03-04 16:57:35 +0100

    Create 05. inventory.js

---
+++ JavaScript Fundamentals January 2023/23. Exercise Objects and Classes/Repeat all exercises Object and Classes/05. inventory.js
@@ -0,0 +1,28 @@
+function inventory(arrayInventory) {
+    let heroesArray = []
+
+    class Hero {
+        constructor(name, level, items) {
+            this.name = name;
+            this.level = level;
+            this.items = items
+        }
+    }
+    for (let Heroes of arrayInventory) {
+        let [heroName, heroLevel, heroItems] = Heroes.split(" / ")
+        let hero = new Hero(heroName, heroLevel, heroItems)
+        heroesArray.push(hero)
+    }
+   let sortedHeroes = heroesArray.sort((el1, el2) => el1.level - el2.level)
+
+   sortedHeroes.forEach(element => {
+    console.log(`Hero: ${element.name}`)
+    console.log(`level => ${element.level}`)
+    console.log(`items => ${element.items}`)
+   })
+}
+inventory([
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+])
