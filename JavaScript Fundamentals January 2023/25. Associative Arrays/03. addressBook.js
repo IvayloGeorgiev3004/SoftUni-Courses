af265dc87e2c940af7ef2350cb08fa357971090f
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-03-09 13:17:32 +0100

    Update 03. addressBook.js

--- JavaScript Fundamentals January 2023/25. Associative Arrays/03. addressBook.js
+++ JavaScript Fundamentals January 2023/25. Associative Arrays/03. addressBook.js
@@ -7,9 +7,54 @@ function addressBook(array) {
     });
 
     let sorted = Object.entries(addressBookObj).sort((a, b) => a[0].localeCompare(b[0]))
-    
+
+    sorted.forEach(element => {
+        let [key, value] = element.join(",").split(',')
+        console.log(`${key} -> ${value}`)
+    })
+
 }
-addressBook(['Tim:Doe Crossing',
-    'Bill:Nelson Place',
-    'Peter:Carlyle Ave',
-    'Bill:Ornery Rd'])
+addressBook(['Bob:Huxley Rd',
+    'John:Milwaukee Crossing',
+    'Peter:Fordem Ave',
+    'Bob:Redwing Ave',
+    'George:Mesta Crossing',
+    'Ted:Gateway Way',
+    'Bill:Gateway Way',
+    'John:Grover Rd',
+    'Peter:Huxley Rd',
+    'Jeff:Gateway Way',
+    'Jeff:Huxley Rd'])
+
+function addressBook(array) {
+    let addressBookObj = {}
+
+    array.forEach(element => {
+        let [name, address] = element.split(":")
+        addressBookObj[name] = address;
+    });
+
+    let sorted = Object.entries(addressBookObj).sort(([keyA, ValueA], [keyB, valueB]) => {
+        keyA.localeCompare(keyB)
+    }) //  ДЕСТРУКТУРИРАНЕ
+
+    //     let sorted = Object.entries(addressBookObj).sort((a, b) => a[0].localeCompare(b[0])) 
+
+
+    sorted.forEach(element => {
+        let [key, value] = element.join(",").split(',')
+        console.log(`${key} -> ${value}`)
+    })
+
+}
+addressBook(['Bob:Huxley Rd',
+    'John:Milwaukee Crossing',
+    'Peter:Fordem Ave',
+    'Bob:Redwing Ave',
+    'George:Mesta Crossing',
+    'Ted:Gateway Way',
+    'Bill:Gateway Way',
+    'John:Grover Rd',
+    'Peter:Huxley Rd',
+    'Jeff:Gateway Way',
+    'Jeff:Huxley Rd'])
