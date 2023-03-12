ad158b0b29db2966fe316c9bfc4be7cf20b40cb0
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-03-12 06:44:56 +0100

    Create 04.04. storage.js

---
+++ JavaScript Fundamentals January 2023/25. Associative Arrays/04.04. storage.js
@@ -0,0 +1,23 @@
+function storage(array) {
+    let listOfProducts = new Map()
+
+    for (let commands of array) {
+        let [product, quantity] = commands.split(" ")
+        quantity = Number(quantity)
+        if (!listOfProducts.has(product)) {
+            listOfProducts.set(product, quantity)
+        } else {
+            let currProductQuantity = listOfProducts.get(product)
+            currProductQuantity += quantity
+            listOfProducts.set(product, currProductQuantity)
+        }
+    }
+    for (let [product, quantity] of listOfProducts) {
+        console.log(`${product} -> ${quantity}`)
+    }
+    debugger
+}
+storage(['tomatoes 10',
+    'coffee 5',
+    'olives 100',
+    'coffee 40'])
