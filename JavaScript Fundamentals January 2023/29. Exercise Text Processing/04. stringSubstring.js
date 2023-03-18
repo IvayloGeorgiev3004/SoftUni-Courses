d14374ccbb7cf203954e4d38d97b18fe0d3bfe89
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-03-18 02:28:31 +0100

    Create 04. stringSubstring.js

---
+++ JavaScript Fundamentals January 2023/29. Exercise Text Processing/04. stringSubstring.js
@@ -0,0 +1,35 @@
+// function stringSubstring(word, text) {
+//     let textArr = text.split(" ")
+//     let wordToLowerCase = word.toLowerCase()
+//     let trigger = false;
+
+//     for (let singleWords of textArr) {
+//         singleWords = singleWords.toLowerCase()
+//         if (singleWords === wordToLowerCase) {
+//             console.log(word)
+//             trigger = true;
+//             break;
+//         }
+//     }
+
+//     if (trigger === false) {
+//         console.log(`${word} not found!`)
+//     }
+// }
+// stringSubstring('python',
+//     'JavaScript is the best programming language')
+
+function stringSubstring(word, text) {
+
+    let newWord = word.toLowerCase()
+    let newText = text.toLowerCase()
+    newText.includes(newWord)
+    if (newText.includes(newWord)) {
+        console.log(word)
+    } else {
+        console.log(`${word} not found!`)
+    }
+
+}
+stringSubstring('python',
+    'pythonn is the best programming language')
