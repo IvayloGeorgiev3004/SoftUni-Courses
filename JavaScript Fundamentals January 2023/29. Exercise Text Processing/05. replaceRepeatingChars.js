bb387e41321d5c3a6b592a1d8e8f771a39e8ffd1
Ivaylo Georgiev <ivaylo.georgiev3004@gmail.com>
2023-03-18 14:54:42 +0100

    Update 05. replaceRepeatingChars.js

--- JavaScript Fundamentals January 2023/29. Exercise Text Processing/05. replaceRepeatingChars.js
+++ JavaScript Fundamentals January 2023/29. Exercise Text Processing/05. replaceRepeatingChars.js
@@ -1,19 +1,16 @@
 function replaceRepeatingChars(string) {
-    let counter = 0;
-    let currentChar = string[0]
-    
-    for (let i = 0; i < string.length; i++) {
-        
-        if (currentChar === string[i]) {
-            counter++
-        } else {
-            for (let i = 0; i < counter-1; i++) {
-                string = string.replace(currentChar,"")
-            }
-            currentChar = string[i]
-            counter = 0;
+
+    let finalResult = ""
+    let currentChar = ""
+    for (let char of string) {
+        if (currentChar !== char) {
+            finalResult += char;
         }
+        currentChar = char
+
     }
-    debugger
+    console.log(finalResult)
 }
 replaceRepeatingChars('aaaaabbbbbcdddeeeedssaa')
+
+
