5df0b221dcead0155bf18db094c06faec82ffbd7
Ivaylo Georgiev <ivaylo.georgiev3004@gmail.com>
2023-03-23 23:57:02 +0100

    Update 01. furniture.js

--- JavaScript Fundamentals January 2023/32. Exercise Regular Expressions/01. furniture.js
+++ JavaScript Fundamentals January 2023/32. Exercise Regular Expressions/01. furniture.js
@@ -1,5 +1,20 @@
-function furniture(){
-let regex = />>[A-Z]+[a-z]*<<[.|\w]+!\w+/g
+function furniture(array) {
+    let regex = />>(?<furniture>[A-Z]+[a-z]*)<<(?<price>[.|\w]+)!(?<quantity>\w+)/g
+    let filteredString = array.join(" ")
+    let sum = 0;
+    let result = regex.exec(filteredString)
 
+    console.log(`Bought furniture: `)
+    while (result !== null) {
+        let price = Number(result.groups.price * result.groups.quantity)
+        sum += price
+        console.log(result.groups.furniture)
+        result = regex.exec(filteredString)
+    }
+    console.log(`Total money spend: ${sum.toFixed(2)}`)
 }
-furniture()
+furniture(['>>Sofa<<312.23!3',
+    '>>TV<<300!5',
+    '>Invalid<<!5',
+    'Purchase']
+)
