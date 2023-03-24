e688a335ea39c8066af7366950a6d9a1ff35fef2
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-03-24 16:19:30 +0100

    Update 04. starEnigma.js

--- JavaScript Fundamentals January 2023/32. Exercise Regular Expressions/04. starEnigma.js
+++ JavaScript Fundamentals January 2023/32. Exercise Regular Expressions/04. starEnigma.js
@@ -1,13 +1,11 @@
 function starEnigma(array) {
     let count = Number(array.shift())
     let countRegex = /[SsTtAaRr]/g
-    let decryptedMessage = /@(?<planetName>[A-Z]+[a-z]*)[\w]*:(?<population>[\d]+)!(?<attackType>[A|D]+)!->(?<soldierCount>[\d]+)/g
-    let message = ""
-    let aCounter = 0;
-    let dCounter = 0;
+    let decryptedMessage = /@(?<planetName>[A-Z]+[a-z]*)[^@|\-|!|:|>|]*:(?<population>[\d]+)[^@|\-|!|:|>|]*!(?<attackType>[A|D]+)![^@|\-|!|:|>|]*->(?<soldierCount>[\d]+)/g
     let finalResObj = {}
 
     for (let i = 0; i < count; i++) {
+        let message = ""
         let match = array[i].match(countRegex)
         let numToRemoveFromASCII = match.length;
         for (let chars of array[i]) {
@@ -15,19 +13,42 @@ function starEnigma(array) {
             let charToAdd = String.fromCharCode(currentCharCode)
             message += charToAdd
         }
+        debugger
         let finalMatch = decryptedMessage.exec(message)
-        if (finalMatch.groups.attackType === "A") {
-            aCounter++
-            finalResObj[finalMatch.groups.planetName] = finalMatch.groups.attackType
-        } else {
-            dCounter++
-            finalResObj[finalMatch.groups.planetName] = finalMatch.groups.attackType
+        if (finalMatch !== null) {
+
+            if (finalMatch.groups.attackType === "A") {
+                finalResObj[finalMatch.groups.planetName] = finalMatch.groups.attackType
+            } else {
+                finalResObj[finalMatch.groups.planetName] = finalMatch.groups.attackType
+            }
+            finalMatch = decryptedMessage.exec(message)
         }
     }
-    debugger
+    let finalArr = Object.entries(finalResObj).sort(([name], [name2]) => {
+        return name.localeCompare(name2)
 
+    })
+    let attackedPlanets = [];
+    let destroyedPlanets = [];
+    for (let planets of finalArr) {
+        if (planets[1] === "A") {
+            attackedPlanets.push(planets[0])
+        } else {
+            destroyedPlanets.push(planets[0])
+        }
+    }
+    console.log(`Attacked planets: ${attackedPlanets.length}`)
+    attackedPlanets.forEach(element => {
+        console.log(`-> ${element}`)
+    })
 
+    console.log(`Destroyed planets: ${destroyedPlanets.length}`)
+    destroyedPlanets.forEach(element => {
+        console.log(`-> ${element}`)
+    })
 }
-starEnigma(['2',
-    'STCDoghudd4=63333$D$0A53333',
+starEnigma(['3',
+    "tt(''DGsvywgerx>6444444444%H%1B9444",
+    'GQhrr|A977777(H(TTTT',
     'EHfsytsnhf?8555&I&2C9555SR'])
