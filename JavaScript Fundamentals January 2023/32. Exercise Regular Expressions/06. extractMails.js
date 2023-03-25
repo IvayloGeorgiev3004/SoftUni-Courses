c603497c37fc0d0321b418d51b0204fda4d5c2b7
iNfRaSoNiC <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-03-25 18:56:44 +0100

    Create 06. extractMails.js

---
+++ JavaScript Fundamentals January 2023/32. Exercise Regular Expressions/06. extractMails.js
@@ -0,0 +1,10 @@
+function extractMails(string) {
+
+    let regex = /(^|(?<=\s))(([a-zA-Z0-9]+)([\.\-_]?)([A-Za-z0-9]+)(@)([a-zA-Z]+([\.\-][A-Za-z]+)+))(\b|(?=\s))/g
+    let match = string.match(regex)
+    match.forEach(element => {
+        console.log(element)
+    });
+
+}
+extractMails(`Just send email to s.miller@mit.edu and j.hopking@york.ac.uk for more information.`)
