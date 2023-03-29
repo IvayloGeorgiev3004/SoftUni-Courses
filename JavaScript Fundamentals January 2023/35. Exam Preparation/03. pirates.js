136b20273633ee60318216b378b4619619a98a08
Ivaylo Georgiev <ivaylo.georgiev3004@gmail.com>
2023-03-29 20:57:09 +0200

    Update 03. pirates.js

--- JavaScript Fundamentals January 2023/35. Exam Preparation/03. pirates.js
+++ JavaScript Fundamentals January 2023/35. Exam Preparation/03. pirates.js
@@ -1,20 +1,4 @@
 function pirates(array) {
-    function createNewCities(cityName, cityPopulation, cityGold) {
-
-
-        let citiesInfo = {
-            name: null,
-            population: null,
-            gold: null
-        }
-        if (this.name !== cityName) {
-            this.name = cityName;
-            this.population = cityPopulation;
-            this.gold = cityGold;
-        } else {
-            console.log(`test`)
-        }
-    }
     let index = 0;
     let createCities = array[index];
     let map = new Map()
@@ -22,31 +6,77 @@ function pirates(array) {
         let [cityName, cityPopulation, cityGold] = createCities.split("||")
         cityGold = Number(cityGold);
         cityPopulation = Number(cityPopulation);
-        // if (citiesInfo.name !== cityName) {
-        //     citiesInfo.name = cityName;
-        //     citiesInfo.population = cityPopulation;
-        //     citiesInfo.gold = cityGold
-        //     map.set(cityName, citiesInfo);
-        // } else {
-        //     let newGoldAmount = citiesInfo.gold + cityGold
-        //     let newPopulationAmount = citiesInfo.population + cityPopulation
-        //     citiesInfo.population = newPopulationAmount;
-        //     citiesInfo.gold = newGoldAmount
-        //     map.set(cityName, citiesInfo);
-        // }
-        createNewCities(cityName, cityPopulation.cityGold);
-        debugger
-
+        let citiesInfo = {
+            name: null,
+            population: null,
+            gold: null,
+        }
+        if (!map.has(cityName)) {
+            citiesInfo.name = cityName;
+            citiesInfo.population = cityPopulation;
+            citiesInfo.gold = cityGold
+            map.set(cityName, citiesInfo);
+        } else {
+            let city = map.get(cityName);
+            let newGoldAmount = city.gold + cityGold
+            let newPopulationAmount = city.population + cityPopulation
+            city.population = newPopulationAmount;
+            city.gold = newGoldAmount
+            map.set(cityName, city);
+        }
         index++
         createCities = array[index];
     }
+    index++
+    let element = array[index]
+    while (element !== "End") {
+        let tokens = element.split(`=>`);
+        let command = tokens.shift();
+        switch (command) {
+            case "Plunder": {
+                let plunderedCity = tokens.shift();
+                let killedPeople = Number(tokens.shift());
+                let goldTaken = Number(tokens.shift());
+                let city = map.get(plunderedCity);
+                city.population -= killedPeople;
+                city.gold -= goldTaken;
+                console.log(`${plunderedCity} plundered! ${goldTaken} gold stolen, ${killedPeople} citizens killed.`)
+                if (city.population <= 0 || city.gold <= 0) {
+                    console.log(`${plunderedCity} has been wiped off the map!`)
+                    map.delete(plunderedCity)
+                }
+                break;
+            }
+            case "Prosper": {
+                let prosperedCity = tokens.shift();
+                let goldAdded = Number(tokens.shift());
+                if (goldAdded <= 0) {
+                    console.log(`Gold added cannot be a negative number!`)
+                } else {
+                    let city = map.get(prosperedCity);
+                    city.gold += goldAdded;
+                    console.log(`${goldAdded} gold added to the city treasury. ${prosperedCity} now has ${city.gold} gold.`)
+                }
+                break;
+            }
+
+        }
+        index++
+        element = array[index]
+    }
+
+    if (map.size > 0) {
+        console.log(`Ahoy, Captain! There are ${map.size} wealthy settlements to go to:`)
+        for (let results of map) {
+            console.log(`${results[0]} -> Population: ${results[1].population} citizens, Gold: ${results[1].gold} kg`)
+        }
+    } else {
+        console.log(`Ahoy, Captain! All targets have been plundered and destroyed!`)
+    }
+
 }
 
-pirates((["Tortuga||345000||1250",
-    "Santo Domingo||240000||630",
-    "Havana||410000||1100",
-    "Havana||410000||1100",
+pirates(["Tortuga||345000||1250",
     "Sail",
-    "Plunder=>Tortuga=>75000=>380",
-    "Prosper=>Santo Domingo=>180",
-    "End"]))
+    "Plunder=>Tortuga=>345000=>1250",
+    "End"])
