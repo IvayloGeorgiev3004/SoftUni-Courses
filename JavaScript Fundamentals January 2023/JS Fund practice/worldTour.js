415b04bddba44e61e5b9e1933916d86314ab8a7a
Ivaylo Georgiev <ivaylo.georgiev3004@gmail.com>
2023-05-01 19:59:28 +0200

    Update worldTour.js

--- JavaScript Fundamentals January 2023/JS Fund practice/worldTour.js
+++ JavaScript Fundamentals January 2023/JS Fund practice/worldTour.js
@@ -1,7 +1,8 @@
 function worldTour(array) {
     let plannedStops = array.shift();
-
-    for (let tokens of array) {
+    let index = 0;
+    let tokens = array[index]
+    while (tokens !== "Travel") {
         let commands = tokens.split(":");
         let command = commands.shift()
         switch (command) {
@@ -18,10 +19,18 @@ function worldTour(array) {
                 console.log(plannedStops)
                 break;
             case "Switch":
-                //TODO
+                debugger;
+                let stringToFind = commands[0];
+                let replaceString = commands[1];
+                switchString(stringToFind, replaceString);
+                console.log(plannedStops)
                 break;
         }
+        index++
+        tokens = array[index];
     }
+
+    console.log(`Ready for world tour! Planned stops: ${plannedStops}`)
 
     function addStop(index, string) {
         if (index >= 0 && index <= plannedStops.length) {
@@ -30,12 +39,18 @@ function worldTour(array) {
     }
     function removeStop(startIndex, endIndex) {
         if (startIndex >= 0 && startIndex <= plannedStops.length && startIndex < endIndex && endIndex > 0 && endIndex <= plannedStops.length) {
-            return plannedStops = plannedStops.substring(0, startIndex) + plannedStops.substring(endIndex+1, plannedStops.len)
+            return plannedStops = plannedStops.substring(0, startIndex) + plannedStops.substring(endIndex + 1, plannedStops.len)
+        }
+    }
+    function switchString(oldString, newString) {
+        if (plannedStops.includes(oldString)) {
+            let stringToAdd = plannedStops.split(oldString)
+            return plannedStops = stringToAdd.join(newString)
         }
     }
 }
-worldTour(["Hawai::Cyprys-Greece",
-    "Add Stop:7:Rome",
-    "Remove Stop:11:16",
+worldTour(["Hawai::Cyprys-GreeceHawaiHawaiHawaiHawaiHawaiHawai",
+    "Add Stop:6:Liverpool",
+    "Remove Stop:13:12",
     "Switch:Hawai:Bulgaria",
     "Travel"])
