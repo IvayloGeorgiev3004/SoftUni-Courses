db3312ef7700f73a6fff7e1398e47f11da8cbb2c
Ivaylo Georgiev <ivaylo.georgiev3004@gmail.com>
2023-07-26 16:38:29 +0200

    Update 01. burgerBus.js

--- JavaScript Fundamentals January 2023/21. Regular Mid Exam/01. burgerBus.js
+++ JavaScript Fundamentals January 2023/21. Regular Mid Exam/01. burgerBus.js
@@ -1,69 +1,52 @@
 function burgerBus(array) {
-    let numberOfCities = Number(array.shift())
-    let profit = 0;
-    let expenses = 0;
-    let moneyEarnedPerCity = 0;
-    let totalMoneyEarned = 0
+    const numberOfCities = Number(array.shift());
+    let totalMoneyEarned = 0;
     let cityCounter = 0;
 
-
     for (let i = 0; i < array.length; i += 3) {
-        cityCounter++
-        let currentCity = array[i]
-        profit = Number(array[i + 1])
-        expenses = Number(array[i + 2])
+        cityCounter++;
+        const currentCity = array[i];
+        let profit = Number(array[i + 1]);
+        let expenses = Number(array[i + 2]);
 
+        // Adjust expenses based on cityCounter conditions
         if (cityCounter % 3 === 0) {
             if (cityCounter % 5 !== 0) {
-                expenses = expenses * 1.50;
+                expenses *= 1.50;
             }
-
         }
+
+        // Adjust profit based on cityCounter conditions
         if (cityCounter % 5 === 0) {
-            profit *= 0.90
+            profit *= 0.90;
         }
 
-        moneyEarnedPerCity = (profit - expenses)
-        console.log(`In ${currentCity} Burger Bus earned ${moneyEarnedPerCity.toFixed(2)} leva.`)
+        const moneyEarnedPerCity = profit - expenses;
+        console.log(`In ${currentCity}, Burger Bus earned ${moneyEarnedPerCity.toFixed(2)} leva.`);
         totalMoneyEarned += moneyEarnedPerCity;
-
     }
 
-    console.log(`Burger Bus total profit: ${totalMoneyEarned.toFixed(2)} leva.`)
-
+    console.log(`Burger Bus total profit: ${totalMoneyEarned.toFixed(2)} leva.`);
 }
-burgerBus(["5",
 
+burgerBus([
+    "5",
     "Lille",
-
     "2226.00",
-
     "1200.60",
-
     "Rennes",
-
     "6320.60",
-
     "5460.20",
-
     "Reims",
-
     "600.20",
-
     "452.32",
-
     "Bordeaux",
-
     "6925.30",
-
     "2650.40",
-
     "Montpellier",
-
     "680.50",
-
-    "290.20"])
-
+    "290.20"
+]);
 
 
 
