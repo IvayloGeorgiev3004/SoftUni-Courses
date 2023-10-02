acc3473100fcb1b52e271d35e41d2918401a1268
Ivaylo Georgiev <ivaylo.georgiev3004@gmail.com>
2023-10-02 14:25:16 +0200

    delete launch

--- JavaScript Advanced May 2023/02. Exercise Syntax, Functions and Statements/9. Words Uppercase.js
+++ JavaScript Advanced May 2023/02. Exercise Syntax, Functions and Statements/9. Words Uppercase.js
@@ -2,7 +2,6 @@ function extractTextToUpperCase(string) {
     let patternRegExp = /\b\w+\b/g
     let matchArr = string.match(patternRegExp)
     let neededWord = ""
-    console.log(typeof matchArr)
 
     for (const word of matchArr) {
         neededWord += `${word.toUpperCase()},`
