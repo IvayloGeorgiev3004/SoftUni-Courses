9d8f6a276a655fb33c102dec599cd91473bcc51c
Ivaylo Georgiev <ivaylo.georgiev3004@gmail.com>
2023-10-11 15:40:03 +0200

    cc

--- JavaScript Advanced May 2023/03. Arrays and Nested Arrays/10. Diagonal Sums2.js
+++ JavaScript Advanced May 2023/03. Arrays and Nested Arrays/10. Diagonal Sums2.js
@@ -1,14 +1,13 @@
 function findDiagonalSums(matrix) {
     let mainDiagonal = 0;
     let secondDiagonal = 0;
-    let firstIndex = 0;
     let lastIndex = matrix.length - 1
 
     for (let i = 0; i < matrix.length; i++) {
-        mainDiagonal += matrix[i][firstIndex++]
+        mainDiagonal += matrix[i][i]
         secondDiagonal += matrix[i][lastIndex--]
     }
     return `${mainDiagonal} ${secondDiagonal}`
 }
-findDiagonalSums([[3, 5, 17], [-1, 7, 14],
-[1, -8, 89]])
+console.log(findDiagonalSums([[3, 5, 17], [-1, 7, 14],
+[1, -8, 89]]))
---
+++ JavaScript Advanced May 2023/04. Exercise Arrays and Nested Arrays/10. Tic-Tac-Toe.js
@@ -0,0 +1,29 @@
+function ticTacToe(movesArr) {
+
+
+
+    let board = [[false, false, false],
+    [false, false, false],
+    [false, false, false]]
+
+    for (let moves of movesArr) {
+        let playerXMove = Number(moves.split(" ")[0])
+        let player0Move = moves.split(" ")[1]
+        debugger
+        
+    }
+
+    debugger
+
+
+}
+ticTacToe(["0 1",
+    "0 0",
+    "0 2",
+    "2 0",
+    "1 0",
+    "1 1",
+    "1 2",
+    "2 2",
+    "2 1",
+    "0 0"])
