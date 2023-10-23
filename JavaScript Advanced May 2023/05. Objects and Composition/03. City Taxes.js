b43f4520471189dc7d843c1fd44f75c53bdecec0
Ivaylo Georgiev <ivaylo.georgiev3004@gmail.com>
2023-10-23 13:05:26 +0200

    Object with functions inside

---
+++ JavaScript Advanced May 2023/05. Objects and Composition/03. City Taxes.js
@@ -0,0 +1,22 @@
+function cityTaxes(name, population, treasury) {
+    let obj = {
+        CityName: name,
+        CityPopulation: population,
+        CityTreasury: treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.CityTreasury += (this.CityPopulation * this.taxRate)
+        }
+
+
+    }
+
+    return obj
+}
+const city =
+    cityTaxes('Tortuga',
+        7000,
+        15000);
+console.log(city);
+city.collectTaxes()
+console.log(city);
