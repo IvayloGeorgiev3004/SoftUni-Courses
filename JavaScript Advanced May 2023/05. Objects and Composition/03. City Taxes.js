db786918ff6a33584066920bcaeb6633b062e9b2
Ivaylo Georgiev <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-10-24 07:18:18 +0200

    Update 03 city taxes

--- JavaScript Advanced May 2023/05. Objects and Composition/03. City Taxes.js
+++ JavaScript Advanced May 2023/05. Objects and Composition/03. City Taxes.js
@@ -1,22 +1,18 @@
 function cityTaxes(name, population, treasury) {
-    let obj = {
-        CityName: name,
-        CityPopulation: population,
-        CityTreasury: treasury,
-        taxRate: 10,
-        collectTaxes() {
-            this.CityTreasury += (this.CityPopulation * this.taxRate)
-        }
+	let obj = {
+		CityName: name,
+		CityPopulation: population,
+		CityTreasury: treasury,
+		taxRate: 10,
+		collectTaxes() {
+			this.CityTreasury += this.CityPopulation * this.taxRate;
+		},
+		applyGrowth(percentage) {},
+	};
 
-
-    }
-
-    return obj
+	return obj;
 }
-const city =
-    cityTaxes('Tortuga',
-        7000,
-        15000);
+const city = cityTaxes("Tortuga", 7000, 15000);
 console.log(city);
-city.collectTaxes()
+city.collectTaxes();
 console.log(city);
