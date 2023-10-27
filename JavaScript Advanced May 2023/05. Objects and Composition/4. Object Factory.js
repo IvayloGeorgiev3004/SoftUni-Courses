e3dbec9122bb7970fdbabd107cf892c3653475ef
Ivaylo Georgiev <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-10-27 20:22:27 +0200

    added 4 object factory and object.assign()

--- JavaScript Advanced May 2023/05. Objects and Composition/4. Object Factory.js
+++ JavaScript Advanced May 2023/05. Objects and Composition/4. Object Factory.js
@@ -1,4 +1,31 @@
-function objectFactory(library, orders) {}
+function factory(library, orders) {
+    // Initialize an empty array to store the final products
+    let ordersArray = [];
+
+    // Iterate through each order in the orders array
+    for (const order of orders) {
+        // Create a new object by copying the template from the current order
+        let finalObject = Object.assign({}, order.template);
+        // ^^^ Creates a new object to store the modified properties of the current order template
+
+        // Iterate through each part in the current order
+        for (command of order.parts) {
+            // Add a property to the finalObject by using the part as the key
+            // Assign the corresponding function from the library to the property
+            finalObject[command] = library[command];
+            // ^^^ Assigns the function from the library to the property of the finalObject
+        }
+
+        // Add the modified object (product) to the ordersArray
+        ordersArray.push(finalObject);
+        // ^^^ Pushes the finalObject representing the current product into the ordersArray
+    }
+
+    // Return the array containing the final products
+    return ordersArray;
+}
+
+// Define a library object containing various functions
 const library = {
     print: function () {
         console.log(`${this.name} is printing a page`);
@@ -10,6 +37,8 @@ const library = {
         console.log(`${this.name} is playing '${track}' by ${artist}`);
     },
 };
+
+// Define an array of orders, where each order specifies a template and a list of parts
 const orders = [
     {
         template: { name: "ACME Printer" },
@@ -28,3 +57,10 @@ const orders = [
         parts: ["play"],
     },
 ];
+
+// Call the factory function with the library and orders as arguments
+// Store the resulting products in the products variable
+const products = factory(library, orders);
+
+// Log the products to the console in a table format
+console.table(products);
