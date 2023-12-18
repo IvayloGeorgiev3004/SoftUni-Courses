e935d21decf6757171a16f8cd69b19f5daddda69
Ivaylo Georgiev <110338294+iNfRaS0NiC@users.noreply.github.com>
2023-12-18 06:47:52 +0100

    DOM solutions

--- JavaScript Advanced May 2023/08. Exercise DOM Introduction/DEMO DOM-Introduction-Exercise-Resources-New/Training DOM/06. Format the Text/solution.js
+++ JavaScript Advanced May 2023/08. Exercise DOM Introduction/DEMO DOM-Introduction-Exercise-Resources-New/Training DOM/06. Format the Text/solution.js
@@ -1,3 +1,14 @@
 function solve() {
-  //TODO
+    let input = document.getElementById("input").value;
+    let regex = /[.]+[ ]+/g;
+    let matches = input.match(regex)
+    let sentences = matches.length + 1;
+
+    let finalOutput = ""
+    if (sentences <=3){
+      finalOutput = `<p>${input}</p>`
+    }
+
+
+    document.getElementById("output").innerHTML = finalOutput
 }
