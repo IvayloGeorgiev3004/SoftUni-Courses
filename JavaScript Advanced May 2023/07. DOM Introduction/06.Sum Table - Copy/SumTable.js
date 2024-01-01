1a723578c8020ff7371e2e87ac33d99cf5931728
Ivaylo Georgiev <110338294+iNfRaS0NiC@users.noreply.github.com>
2024-01-01 20:18:01 +0100

    DOM manipulation

--- JavaScript Advanced May 2023/07. DOM Introduction/06.Sum Table - Copy/SumTable.js
+++ JavaScript Advanced May 2023/07. DOM Introduction/06.Sum Table - Copy/SumTable.js
@@ -1,24 +1,15 @@
 function sumTable() {
-  // Получаване на всички елементи <td> в таблицата
-  let elements = document.getElementsByTagName("td");
+    let elements = document.querySelectorAll("td");
+    let finalResult = document.getElementById("sum");
+    let sum = 0;
 
-  // Инициализация на променливи за индекс и обща сума
-  let index = 0;
-  let sum = 0;
-
-  // Използване на цикъл while за обхождане на елементите <td>
-  while (index < elements.length - 1) {
-    // Проверка дали индексът (index) е нечетен
-    if (index % 2 !== 0) {
-      // Ако индексът е нечетен, добавяне на стойността на текущия елемент <td> към сумата
-      sum += Number(elements[index].textContent);
+    for (let i = 0; i < elements.length; i++) {
+        if (i === 1 || i == 3 || i === 5) {
+            let currentValue = elements[i].textContent;
+            currentValue = Number(currentValue);
+            sum += currentValue;
+        }
     }
-    // Увеличаване на индекса
-    index++;
-  }
 
-  // Задаване на сумата като текстово съдържание на последния елемент <td>
-  elements[elements.length - 1].textContent = sum;
+    finalResult.textContent = sum;
 }
-
-
--- JavaScript Advanced May 2023/08. Exercise DOM Introduction/Training DOM Introduction/08. Generate Report/report.js
+++ JavaScript Advanced May 2023/08. Exercise DOM Introduction/Training DOM Introduction/08. Generate Report/report.js
@@ -1,3 +1,28 @@
 function generateReport() {
-    //TODO
+    // const checkedElements = document.querySelectorAll('input[type="checkbox"]:checked')
+
+    let tableHeaders = document.querySelectorAll("thead tr th");
+    let tableColumns = document.querySelectorAll("tbody tr");
+
+    let clickedHeaders = [];
+
+    for (let tableHead of tableHeaders) {
+        if (tableHead.firstElementChild.checked === true) {
+            clickedHeaders.push(tableHead);
+        }
+    }
+
+    let finalResult = [];
+    for (let column of tableColumns) {
+        let finalObj = {};
+        for (let clicked of clickedHeaders) {
+            let neededIndex = clicked.cellIndex;
+            let header = clicked.firstElementChild.name
+            let value = column.cells[neededIndex].textContent;
+            finalObj[header] = value;
+        }
+        finalResult.push(finalObj);
+    }
+    let result = JSON.stringify(finalResult, null, 2);
+    document.getElementById("output").value = result;
 }
